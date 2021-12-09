8d2b07316250fc3b06de4411cc23f7c0abe04d61
Tiago Gomes <tidgomes@gmail.com>
2021-12-09 17:09:57 -0300

    sending a simple response

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require("http");
+
+const products = require("./data/products.json");
+
+const PORT = process.env.PORT || 5000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify(products));
+});
+
+server.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
